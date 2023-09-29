24b292c4226eb3425192a712e29d9d4e2b074763
Jean Osco <willjean29@gmail.com>
2023-09-29 13:02:29 -0500

    init: add initial config to project

---
+++ blog-posts/src/App.jsx
@@ -0,0 +1,13 @@
+import { Posts } from "./Posts";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Blog Posts</h1>
+      <Posts />
+    </div>
+  )
+}
+
+export default App
